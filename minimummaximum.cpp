ed31e9d84484c62ed0ac2eadcdd7ae93a89027e0
Sampathtech <95083027+Sampathtech@users.noreply.github.com>
2021-11-29 13:56:13 +0530

    Add files via upload

--- minimummaximum.cpp
+++ minimummaximum.cpp
@@ -7,10 +7,17 @@ main()
 	printf("enter another number");
 	scanf("%f",&b);
 	if (a>b){
-		printf("the maximum number is %f",a);
+		printf("the maximum number is %f\n",a);
 	}
 	
 	else {
-		printf("the maximum number is %f",b);
+		printf("the maximum number is %f\n",b);
+	}
+		if (a>b){
+		printf("the minimum number is %f\n",b);
+	}
+	
+	else {
+		printf("the minimum number is %f\n",a);
 	}
 }
