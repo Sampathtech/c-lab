603f85855c1ff12c22a5d24da44c5b466f9e061e
Sampathtech <95083027+Sampathtech@users.noreply.github.com>
2021-12-01 13:29:12 +0530

    C LAB

---
+++ divisiblebyboth3and5.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter any number");
+	scanf("%d",&a);
+	if (a%3==0 && a%5==0){
+		printf("number is divisible by 3 & 5\n");
+	}
+	else{
+		printf("number is not divisible by both 3 & 5\n");
+	}
+}
