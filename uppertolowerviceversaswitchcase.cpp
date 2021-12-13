e6e326285b3200160554ec4b7b81206393a1a485
Sampathtech <95083027+Sampathtech@users.noreply.github.com>
2021-12-13 14:40:46 +0530

    Add files via upload

---
+++ uppertolowerviceversaswitchcase.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	char c;
+	printf("enter a character");
+	scanf("%c",&c);
+	switch(c>=65 && c<=90)
+	{
+		case 1:
+			c=c+32;
+			printf("the character converted %c",c); break;
+		case 0:
+			c = c-32;
+			printf("the character converted %c",c); break;
+	}
+}
