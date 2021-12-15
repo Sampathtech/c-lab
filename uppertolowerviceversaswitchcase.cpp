b7659e1a89cfa12583f0ff0b9eec8aa195d25ffe
Sampathtech <95083027+Sampathtech@users.noreply.github.com>
2021-12-15 13:36:58 +0530

    Add files via upload

--- uppertolowerviceversaswitchcase.cpp
+++ uppertolowerviceversaswitchcase.cpp
@@ -9,10 +9,20 @@ main()
 		case 1:
 			c=c+32;
 			printf("the character converted %c",c); break;
-		case 0:
-			c = c-32;
-			printf("the character converted %c",c); break;
-		default:
-			printf("the character entered is a number"); break;
+		case 0: switch(c>=97 && c<=122){
+				case 1:
+					c = c-32;
+					printf("the character converted %c",c); break;
+					break;
+				case 0:switch(c>=0 && c<=9)
+				{
+						default:
+						printf("the character entered is a number"); break;
+				}
+				
+									}
+			
+		
+		
 	}
 }
